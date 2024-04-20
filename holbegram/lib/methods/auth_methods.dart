e7a910ee2cbf579d1cc8f9c3705d06631e6fc4bb
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2024-04-20 16:17:22 +0100

    methods

--- holbegram/lib/methods/auth_methods.dart
+++ holbegram/lib/methods/auth_methods.dart
@@ -2,60 +2,88 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthMethods {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+import 'package:holbegram/models/user.dart';
 
-  Future<String> login({required String email, required String password}) async {
-    if (email.isEmpty || password.isEmpty) {
-      return 'Please fill all the fields';
+
+class AuthMethode {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+    Future<String> login({required String email,required String password}) async {
+        if (email.isEmpty || password.isEmpty) {
+            return 'Please fill all the fields';
+        }
+        try {
+            var userCredential = await _auth.signInWithEmailAndPassword(
+                email: email,
+                password: password,
+            );
+            var user = userCredential.user;
+            return 'Success';
+        } catch (e) {
+            return e.toString();
+        }
     }
 
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return 'success';
-    } catch (e) {
-      return e.toString();
-    }
-  }
+    Future<String> signUpUser({
+        required String email,
+        required String password,
+        required String username,
+        Uint8List? file,
+        }) async {
+        if (email.isEmpty || password.isEmpty || username.isEmpty) {
+            return 'Please fill all the fields';
+        }
+        try {
+            UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+                email: email,
+                password: password,
+            );
+            User? user = userCredential.user;
 
-  Future<String> signUpUser({
-    required String email,
-    required String password,
-    required String username,
-    Uint8List? file,
-  }) async {
-    if (email.isEmpty || password.isEmpty || username.isEmpty) {
-      return 'Please fill all the fields';
-    }
+            Users users = Users(
+                email: email,
+                username: username,
+                uid: user!.uid,
+                bio: '',
+                photoUrl: '',
+                followers: [],
+                following: [],
+                posts: [],
+                saved: [],
+                searchKey: '',
+            );
 
-    try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+            await _firestore.collection('users').doc(user.uid).set(users.toJson());
+            return 'Success';
+        } catch (e) {
+            return e.toString();
+        }
+        }
 
-      User? user = userCredential.user;
-      if (user != null) {
-        await _firestore.collection("users").doc(user.uid).set({
-          'uid': user.uid,
-          'email': email,
-          'username': username,
-          'bio': '',
-          'photoUrl': '',
-          'followers': [],
-          'following': [],
-          'posts': [],
-          'saved': [],
-          'searchKey': username.toLowerCase(),
-        });
-      }
-      return 'success';
-    } catch (e) {
-      return e.toString();
-    }
-  }
+        Future<Users?> getUserDetails() async {
+            try {
+            final currentUser = FirebaseAuth.instance.currentUser;
+            if (currentUser != null) {
+                final snapshot = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+
+                if (snapshot != null) {
+                    final user = Users.fromSnap(snapshot);
+                    return user;
+                }
+            }
+        } catch (e) {
+            print(e);
+        }
+        return null;
 }
+
+    Future<void> logOut() async {
+        try {
+            await _auth.signOut();
+        } catch (e) {
+            print("Error logging out: $e");
+        }
+    }
+}
+
